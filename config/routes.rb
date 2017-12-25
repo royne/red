8bfe4b796dae528c88d302c6b3b640c694deb064
royne <royne.chaves@gmail.com>
2017-12-25 18:53:53 -0500

    add devise2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :statuses
+
+  root to: 'statuses#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
